4c046e21186885a6e00692c1be86f1d76dd69caa
fazrigading <super.fai700@gmail.com>
2022-04-03 11:34:29 +0800

    Gading Flutter

---
+++ lib/main.dart
@@ -0,0 +1,137 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final ThemeData theme = ThemeData();
+    Color myColor = Theme.of(context).colorScheme.secondary;
+    return MaterialApp(
+      title: "Flutter Gading",
+      theme: theme.copyWith(
+        colorScheme: theme.colorScheme.copyWith(secondary: myColor),
+      ),
+      home: const MyHomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+// ignore: must_be_immutable
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var lebar = MediaQuery.of(context).size.width;
+    var tinggi = MediaQuery.of(context).size.height;
+
+    return Scaffold(
+      body: Container(
+        alignment: Alignment.center,
+        height: tinggi,
+        width: lebar,
+        decoration: const BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.topRight,
+                end: Alignment.bottomLeft,
+                colors: [Colors.blue, Colors.teal])),
+        child: Scaffold(
+          backgroundColor: Colors.transparent,
+          appBar: AppBar(
+            centerTitle: true,
+            title: const Text(
+              "Post-test 1 Gading",
+              style: TextStyle(
+                  fontFamily: 'SanFrancisco', fontWeight: FontWeight.w600),
+              textAlign: TextAlign.center,
+            ),
+            backgroundColor: Colors.black45,
+          ),
+          body: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Image.asset('assets/loginiconsmall.png'),
+              const Padding(
+                  padding: EdgeInsets.only(left: 75, right: 75),
+                  child: Text(
+                    "Sign-in",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 42,
+                        fontFamily: 'SanFrancisco',
+                        fontWeight: FontWeight.w700),
+                  )),
+              const Padding(
+                padding:
+                    EdgeInsets.only(left: 75, right: 75, top: 25, bottom: 25),
+                child: TextField(
+                  style: TextStyle(
+                      color: Colors.white, fontFamily: 'SanFrancisco'),
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.person, color: Colors.white),
+                    focusColor: Colors.white,
+                    focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white)),
+                    border: OutlineInputBorder(),
+                    hintText: 'Username',
+                    hintStyle: TextStyle(
+                        color: Colors.white60,
+                        fontSize: 18,
+                        fontFamily: 'SanFrancisco'),
+                  ),
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.only(left: 75, right: 75, bottom: 25),
+                child: TextField(
+                  obscureText: true,
+                  style: TextStyle(
+                      color: Colors.white, fontFamily: 'SanFrancisco'),
+                  decoration: InputDecoration(
+                      prefixIcon: Icon(Icons.lock, color: Colors.white),
+                      focusColor: Colors.white,
+                      border: OutlineInputBorder(),
+                      focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.white)),
+                      hintText: 'Password',
+                      hintStyle: TextStyle(
+                          color: Colors.white60,
+                          fontSize: 18,
+                          fontFamily: 'SanFrancisco')),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 75, right: 75),
+                child: Material(
+                  elevation: 5.0,
+                  borderRadius: BorderRadius.circular(30.0),
+                  color: const Color.fromARGB(255, 186, 136, 57),
+                  child: MaterialButton(
+                      minWidth: MediaQuery.of(context).size.width,
+                      padding:
+                          const EdgeInsets.fromLTRB(20.0, 20.0, 20.0, 20.0),
+                      onPressed: () {},
+                      child: const Text("Login",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                              fontSize: 18,
+                              color: Colors.white,
+                              fontWeight: FontWeight.w700,
+                              fontFamily: 'SanFrancisco'))),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
